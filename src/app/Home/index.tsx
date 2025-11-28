c1857bb62bb677c3473cdd5a6fe530b926cb868c
Dev-Nadson <nadson.pros@gmail.com>
2025-11-28 14:10:33 -0300

    Feat: testando image e buttons

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,13 @@
-import { Text, View, Image, Button } from 'react-native';
-import { styles } from './styles';
+import { Text, View, Image } from 'react-native';
+import { Button } from '@/src/components/Button';
+import { home_styles } from './styles';
 
 function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>hello world!</Text>
-      <Image style={styles.image} source={require("@/assets/home-assets/logo.png")} />
-      <Button title='Compre agora!' />
+    <View style={home_styles.container}>
+      <Text style={home_styles.text}>hello world!</Text>
+      <Image style={home_styles.image} source={require("@/assets/home-assets/logo.png")} />
+      <Button />
     </View>
   );
 }
